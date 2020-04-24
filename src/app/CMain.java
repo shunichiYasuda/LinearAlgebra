009d44e75ff00b465efd7c6ebb8f65c5798fce5c
Shunichi <shunichi.sd@gmail.com>
2020-04-24 17:20:10 +0900

    Apr 24 complete

--- src/app/CMain.java
+++ src/app/CMain.java
@@ -1,5 +1,8 @@
 package app;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 import linearAlgebra.CMatrix;
 import linearAlgebra.CVector_Col;
 import linearAlgebra.CVector_Row;
@@ -7,27 +10,9 @@ import linearAlgebra.CVector_Row;
 public class CMain {
 
 	public static void main(String[] args) {
-		double[] v = { 1, 2, -1 };
-		CVector_Col a = new CVector_Col(v);
-		printVec(a);
-		CVector_Row b = new CVector_Row(new double[] { 1, 3, -2 });
-		printVec(b);
-		System.out.println("-----------------");
-		CMatrix c = a.byVec(b);
-		printMat(c);
-		System.out.println("-----------------");
-		c.setCol(2, a);
-		printMat(c);
-		System.out.println("-----------------");
-		c.setRow(1, b);
-		printMat(c);
-		System.out.println("--------c a matrix by vector---------");
-		printVec(c.byVec(a));
-		System.out.println("--------b c vector by matrix ---------");
-		printVec(b.byMat(c));
 		System.out.println("--------4x4 matrix ---------");
-		double[][] data = {{2,-2,4,2},{2,-1,6,3},{3,-2,12,12},{-1,3,-4,4}};
-		//double[][] data = { { 0, 1, 4, -6 }, { 4, -2, -2, 4 }, { -2, 0, 0, 3 }, { 8, 1, 6, -2 } };
+		//double[][] data = { { 2, -2, 4, 2 }, { 2, -1, 6, 3 }, { 3, -2, 12, 12 }, { -1, 3, -4, 4 } };
+		double[][] data = { { 0, 1, 4, -6 }, { 4, -2, -2, 4 }, { -2, 0, 0, 3 }, { 8,1, 6, -2 } };
 		CMatrix original = new CMatrix(data);
 		printMat(original);
 		System.out.println("-------4x4単位行列 ---------");
@@ -53,14 +38,17 @@ public class CMain {
 		// ここから上三角行列をつくる
 		for (int baseRow = 0; baseRow < original.rowN - 1; baseRow++) {
 			CVector_Row baseRowVec = original.getRow(baseRow);
+			CVector_Row baseRowVecUnit = unit.getRow(baseRow);
 			System.out.println("baseRow =" + baseRow);
 			printVec(baseRowVec);
 			int targetCol = baseRow;
 			double baseValue = baseRowVec.getValue(targetCol);
+			double baseValueUnit = baseRowVecUnit.getValue(targetCol);
 			System.out.println("baseValue = " + baseValue);
 			for (int targetRow = baseRow + 1; targetRow < original.rowN; targetRow++) {
 				// 処理する行を抜き出す
 				CVector_Row targetRowVec = original.getRow(targetRow);
+				CVector_Row targetRowVecUnit = unit.getRow(targetRow);
 				System.out.println("target vector");
 				printVec(targetRowVec);
 				double targetValue = targetRowVec.getValue(targetCol);
@@ -70,21 +58,24 @@ public class CMain {
 				// base行にすべて ratio をかける
 				System.out.println("第" + baseRow + "行 x ratio");
 				CVector_Row tmpVec = baseRowVec.byScalar(ratio);
+				CVector_Row tmpVecUnit = baseRowVecUnit.byScalar(ratio);
 				printVec(tmpVec);
 				System.out.println("第" + targetRow + "行 - 第" + baseRow + "行 x ratio");
 				CVector_Row resultVec = targetRowVec.subtractVec(tmpVec);
+				CVector_Row resultVecUnit = targetRowVecUnit.subtractVec(tmpVecUnit);
 				printVec(resultVec);
 				// 行列の注目行をこの結果に置き換える。
 				original.setRow(targetRow, resultVec);
+				unit.setRow(targetRow, resultVecUnit);
 				System.out.println("結果行列");
-				printMat(original);
+				printMat(unit);
 			} // end of for(int targetRow=baseRow+1
-			
+
 		} // end of for(int baseRow = 0
 		System.out.println("結果行列");
-		printMat(original);
-			// 以上で original 行列は上三角行列になった。
-			// 行列式
+		printMat(unit);
+		// 以上で original 行列は上三角行列になった。
+		// 行列式
 		double det = 1.0;
 		for (int i = 0; i < original.rowN; i++) {
 			det *= original.getValue(i, i);
@@ -92,36 +83,82 @@ public class CMain {
 		det = Math.pow(-1.0, count) * det;
 		System.out.println("det = " + det);
 		System.out.println("----ガウス・ジョルダン法を上三角行列に適用-----");
-		// 
+		//
 		for (int pivot = 0; pivot < original.rowN; pivot++) {
 			// pivot 行、pivot列成分を1 にする。
 			double invPivot = 1.0 / original.getValue(pivot, pivot);
 			CVector_Row pivotVec = original.getRow(pivot);
+			CVector_Row pivotVecUnit = unit.getRow(pivot);
 			pivotVec.byScalarThis(invPivot);
+			pivotVecUnit.byScalarThis(invPivot);
 			original.setRow(pivot, pivotVec);
-			System.out.println("pivot ="+pivot+"行の処理終了後");
-			printMat(original);
-			for(int i=0;i<pivot;i++) {
-				if(i != pivot) {
+			unit.setRow(pivot, pivotVecUnit);
+			System.out.println("pivot =" + pivot + "行の処理終了後");
+			// printMat(original);
+			for (int i = 0; i < pivot; i++) {
+				if (i != pivot) {
 					CVector_Row targetVec = original.getRow(i);
+					CVector_Row targetVecUnit = unit.getRow(i);
 					double value = targetVec.getValue(pivot);
 					CVector_Row tmpVec = pivotVec.byScalar(value);
 					CVector_Row subVec = targetVec.subtractVec(tmpVec);
+					CVector_Row tmpVecUnit = pivotVecUnit.byScalar(value);
+					CVector_Row subVecUnit = targetVecUnit.subtractVec(tmpVecUnit);
 					original.setRow(i, subVec);
+					unit.setRow(i, subVecUnit);
 				}
-				System.out.println("target i ="+i+"行の処理終了後");
-				printMat(original);
+				System.out.println("target i =" + i + "行の処理終了後");
+				printMat(unit);
 			}
+			//
+
 		}
+
+		double[][] tableA = { { -2, 3 }, { 1, -1 } };
+		double[][] tableB = { { -1, 4 }, { 2, -1 } };
+		CMatrix matA = new CMatrix(tableA);
+		CMatrix matB = new CMatrix(tableB);
+		System.out.println("matA");
+		printMat(matA);
+		System.out.println("matB");
+		printMat(matB);
+		CMatrix matC = matA.byMat(matB);
+		System.out.println("--- matA x matB ---");
+		printMat(matC);
 		//
-		
+		CMatrix matD = new CMatrix(data);
+		printMat(matD.byMat(unit));
+		//
+		System.out.println("method テスト");
+		CMatrix permutUnitMatrix = matD.permut();
+		printMat(permutUnitMatrix);
+		System.out.println("det ="+matD.det());
+		//
+		System.out.println("matD");
+		printMat(matD);
+		System.out.println("--------inverse--------");
+		CMatrix invMatD = matD.inverse();
+		printMat(invMatD);
+		System.out.println("check");
+		printMat(matD.byMat(invMatD));
+		//
+		System.out.println("matA");
+		printMat(matA);
+		System.out.println("--------inverse--------");
+		CMatrix invMatA = matA.inverse();
+		printMat(invMatA);
+		System.out.println("check");
+		printMat(matA.byMat(invMatA));
+		System.out.println("det ="+matA.det());
+
 	} // end of main()
 		//
 
 	static void printMat(double[][] in) {
 		for (double[] r : in) {
 			for (double d : r) {
-				System.out.print(d + "\t");
+				BigDecimal a = BigDecimal.valueOf(d);
+				System.out.print(a.setScale(4,RoundingMode.HALF_UP) + "\t");
 			}
 			System.out.print("\n");
 		}
@@ -134,14 +171,14 @@ public class CMain {
 	//
 	static void printVec(CVector_Col in) {
 		for (int i = 0; i < in.getDim(); i++) {
-			System.out.println(in.getValue(i));
+			System.out.println(String.format("%4f", in.getValue(i)));
 		}
 	}
 
 	//
 	static void printVec(CVector_Row in) {
 		for (int i = 0; i < in.getDim(); i++) {
-			System.out.print("\t" + in.getValue(i));
+			System.out.print("\t" + String.format("%.4f",in.getValue(i)));
 		}
 		System.out.println();
 	}
--- src/linearAlgebra/CMatrix.java
+++ src/linearAlgebra/CMatrix.java
@@ -2,198 +2,354 @@ package linearAlgebra;
 
 public class CMatrix {
 	private double[][] mat;
-	public int colN, rowN; //行数、列数
-	public boolean isSquare; //正方行列か？
-	public CMatrix(int m,int n){
-		this.mat = new double[m][n]; //m行ｎ列行列
+	public int colN, rowN; // 行数、列数
+	public boolean isSquare; // 正方行列か？
+
+	public CMatrix(int m, int n) {
+		this.mat = new double[m][n]; // m行ｎ列行列
 		this.rowN = m;
 		this.colN = n;
-		if(m==n) {
+		if (m == n) {
 			this.isSquare = true;
-		}else {
+		} else {
 			this.isSquare = false;
 		}
-		//基本は単位行列
-		for(int i=0;i<m;i++) {
-			for(int j=0;j<n;j++) {
-				if(i==j) {
+		// 基本は単位行列
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (i == j) {
 					this.mat[i][j] = 1.0;
-				}else {
+				} else {
 					this.mat[i][j] = 0.0;
 				}
 			}
 		}
-	} //end of CMatrix(int , int)
+	} // end of CMatrix(int , int)
+
 	public CMatrix(double[][] in) {
 		this.rowN = in.length;
 		this.colN = in[0].length;
 		this.mat = new double[this.rowN][this.colN];
-		for(int i=0;i<this.rowN;i++) {
-			for(int j=0;j<this.colN;j++) {
+		for (int i = 0; i < this.rowN; i++) {
+			for (int j = 0; j < this.colN; j++) {
 				this.mat[i][j] = in[i][j];
 			}
 		}
-		if(this.rowN==this.colN) {
+		if (this.rowN == this.colN) {
 			this.isSquare = true;
-		}else {
+		} else {
 			this.isSquare = false;
 		}
 	} // end of CMatrix(double[][] )
+
 	public CMatrix(CMatrix in) {
 		this.rowN = in.rowN;
 		this.colN = in.colN;
 		this.mat = new double[this.rowN][this.colN];
-		for(int i=0;i<this.rowN;i++) {
-			for(int j=0;j<this.colN;j++) {
+		for (int i = 0; i < this.rowN; i++) {
+			for (int j = 0; j < this.colN; j++) {
 				this.mat[i][j] = in.mat[i][j];
 			}
 		}
-		if(this.rowN==this.colN) {
+		if (this.rowN == this.colN) {
 			this.isSquare = true;
-		}else {
+		} else {
 			this.isSquare = false;
 		}
 	}
-	//転置
+
+	// 転置
 	public CMatrix transpose() {
-		CMatrix r = new CMatrix(this.colN,this.rowN);
-		for(int i=0;i<r.rowN;i++) {
-			for(int j=0;j<r.colN;j++) {
+		CMatrix r = new CMatrix(this.colN, this.rowN);
+		for (int i = 0; i < r.rowN; i++) {
+			for (int j = 0; j < r.colN; j++) {
 				r.mat[i][j] = this.mat[j][i];
 			}
 		}
 		return r;
 	}
-	//行列の足し算
+
+	// 行列の足し算
 	public CMatrix addMat(CMatrix in) {
-		CMatrix r = new CMatrix(this.rowN,this.colN);
-		for(int i= 0;i<this.rowN;i++) {
-			for(int j=0;j<this.colN;j++) {
-				double d = this.mat[i][j]+in.mat[i][j];
+		CMatrix r = new CMatrix(this.rowN, this.colN);
+		for (int i = 0; i < this.rowN; i++) {
+			for (int j = 0; j < this.colN; j++) {
+				double d = this.mat[i][j] + in.mat[i][j];
 				r.mat[i][j] = d;
 			}
 		}
 		return r;
 	}
-	//行列の引き算
+
+	// 行列の引き算
 	public CMatrix subtractMat(CMatrix in) {
-		CMatrix r = new CMatrix(this.rowN,this.colN);
-		for(int i= 0;i<this.rowN;i++) {
-			for(int j=0;j<this.colN;j++) {
-				double d = this.mat[i][j]-in.mat[i][j];
+		CMatrix r = new CMatrix(this.rowN, this.colN);
+		for (int i = 0; i < this.rowN; i++) {
+			for (int j = 0; j < this.colN; j++) {
+				double d = this.mat[i][j] - in.mat[i][j];
 				r.mat[i][j] = d;
 			}
 		}
 		return r;
 	}
-	//行列の定数倍
+
+	// 行列の定数倍
 	public CMatrix byScalar(double in) {
-		CMatrix r = new CMatrix(this.rowN,this.colN);
-		for(int i= 0;i<this.rowN;i++) {
-			for(int j=0;j<this.colN;j++) {
-				double d = this.mat[i][j]*in;
+		CMatrix r = new CMatrix(this.rowN, this.colN);
+		for (int i = 0; i < this.rowN; i++) {
+			for (int j = 0; j < this.colN; j++) {
+				double d = this.mat[i][j] * in;
 				r.mat[i][j] = d;
 			}
 		}
 		return r;
 	}
-	//自分自身を定数倍
+
+	// 自分自身を定数倍
 	public void byScalarThis(double in) {
-		for(int i= 0;i<this.rowN;i++) {
-			for(int j=0;j<this.colN;j++) {
-				double d = this.mat[i][j]*in;
+		for (int i = 0; i < this.rowN; i++) {
+			for (int j = 0; j < this.colN; j++) {
+				double d = this.mat[i][j] * in;
 				this.mat[i][j] = d;
 			}
 		}
 	}
-	//行列かけベクトル。後ろから列ベクトルをかける。
-	//結果はこの行列の行数を次数とする列ベクトル
+
+	// 行列かけベクトル。後ろから列ベクトルをかける。
+	// 結果はこの行列の行数を次数とする列ベクトル
 	public CVector_Col byVec(CVector_Col in) {
 		CVector_Col r = new CVector_Col(this.rowN);
-		for(int i=0;i<this.rowN;i++) {
+		for (int i = 0; i < this.rowN; i++) {
 			CVector_Row row = this.getRow(i);
-			r.setValue(i,row.byVec(in));
+			r.setValue(i, row.byVec(in));
 		}
 		return r;
 	}
-	//この行列の第p列と第q列を入れ替える
-	public void exchangeCol(int p,int q) {
+
+	// 行列のかけ算。後ろからかける
+	public CMatrix byMat(CMatrix in) {
+		CMatrix r = new CMatrix(this.rowN, in.colN);
+		for (int i = 0; i < r.rowN; i++) {
+			for (int j = 0; j < r.colN; j++) {
+				r.mat[i][j] = this.getRow(i).byVec(in.getCol(j));
+			}
+		}
+		return r;
+	}
+
+	// 逆行列計算のため単位置換行列を出力する。
+	public CMatrix permut() {
+		CMatrix original = new CMatrix(this);
+		CMatrix r = new CMatrix(original.rowN, original.colN);
+		// int count = 0;
+		for (int i = 0; i < original.rowN; i++) {
+			int pivotRow = i;
+			int maxpos = original.getCol(pivotRow).hwMaxPos(i);
+			if (maxpos != pivotRow) {
+				original.exchangeRow(pivotRow, maxpos);
+				r.exchangeRow(pivotRow, maxpos);
+				// count++;
+			} // end of if(...)
+		} // end of for(...)
+		return r;
+	}
+	// 行列式。
+	public double det() {
+		CMatrix original = new CMatrix(this);
+		CMatrix r = new CMatrix(this.rowN, this.colN);
+		int count = 0;
+		for (int i = 0; i < original.rowN; i++) {
+			int pivotRow = i;
+			int maxpos = original.getCol(pivotRow).hwMaxPos(i);
+			if (maxpos != pivotRow) {
+				original.exchangeRow(pivotRow, maxpos);
+				r.exchangeRow(pivotRow, maxpos);
+				count++;
+			} // end of if(...)
+		} // end of for(...)
+		// 以上で置換終わり
+		// ここから上三角行列をつくる
+		for (int baseRow = 0; baseRow < original.rowN - 1; baseRow++) {
+			CVector_Row baseRowVec = original.getRow(baseRow);
+			CVector_Row baseRowVecUnit = r.getRow(baseRow);
+			int targetCol = baseRow;
+			double baseValue = baseRowVec.getValue(targetCol);
+			for (int targetRow = baseRow + 1; targetRow < original.rowN; targetRow++) {
+				// 処理する行を抜き出す
+				CVector_Row targetRowVec = original.getRow(targetRow);
+				CVector_Row targetRowVecUnit = r.getRow(targetRow);
+				double targetValue = targetRowVec.getValue(targetCol);
+				double ratio = targetValue / baseValue;
+				// base行にすべて ratio をかける
+				CVector_Row tmpVec = baseRowVec.byScalar(ratio);
+				CVector_Row tmpVecUnit = baseRowVecUnit.byScalar(ratio);
+				CVector_Row resultVec = targetRowVec.subtractVec(tmpVec);
+				CVector_Row resultVecUnit = targetRowVecUnit.subtractVec(tmpVecUnit);
+				// 行列の注目行をこの結果に置き換える。
+				original.setRow(targetRow, resultVec);
+				r.setRow(targetRow, resultVecUnit);
+			} // end of for(int targetRow=baseRow+1
+		} // end of for(int baseRow = 0
+		double det = 1.0;
+		for (int i = 0; i < original.rowN; i++) {
+			det *= original.getValue(i, i);
+		}
+		det = Math.pow(-1.0, count) * det;
+		return det;
+	}// end of det()
+	//逆行列
+	public CMatrix inverse() {
+		CMatrix original = new CMatrix(this);
+		CMatrix unit = new CMatrix(this.rowN, this.colN);
+		for (int i = 0; i < original.rowN; i++) {
+			int pivotRow = i;
+			int maxpos = original.getCol(pivotRow).hwMaxPos(i);
+			if (maxpos != pivotRow) {
+				original.exchangeRow(pivotRow, maxpos);
+				unit.exchangeRow(pivotRow, maxpos);
+			} // end of if(...)
+		} // end of for(...)
+		// 以上で置換終わり。ここから上三角行列をつくる
+		for (int baseRow = 0; baseRow < original.rowN - 1; baseRow++) {
+			CVector_Row baseRowVec = original.getRow(baseRow);
+			CVector_Row baseRowVecUnit = unit.getRow(baseRow);
+			int targetCol = baseRow;
+			double baseValue = baseRowVec.getValue(targetCol);
+			for (int targetRow = baseRow + 1; targetRow < original.rowN; targetRow++) {
+				// 処理する行を抜き出す
+				CVector_Row targetRowVec = original.getRow(targetRow);
+				CVector_Row targetRowVecUnit = unit.getRow(targetRow);
+				double targetValue = targetRowVec.getValue(targetCol);
+				double ratio = targetValue / baseValue;
+				// base行にすべて ratio をかける
+				CVector_Row tmpVec = baseRowVec.byScalar(ratio);
+				CVector_Row tmpVecUnit = baseRowVecUnit.byScalar(ratio);
+				CVector_Row resultVec = targetRowVec.subtractVec(tmpVec);
+				CVector_Row resultVecUnit = targetRowVecUnit.subtractVec(tmpVecUnit);
+				// 行列の注目行をこの結果に置き換える。
+				original.setRow(targetRow, resultVec);
+				unit.setRow(targetRow, resultVecUnit);
+			} // end of for(int targetRow=baseRow+1
+		} // end of for(int baseRow = 0
+		// 以上で original 行列は上三角行列になった。
+		//ガウス・ジョルダン法を上三角行列に適用
+		for (int pivot = 0; pivot < original.rowN; pivot++) {
+			// pivot 行、pivot列成分を1 にする。
+			double invPivot = 1.0 / original.getValue(pivot, pivot);
+			CVector_Row pivotVec = original.getRow(pivot);
+			CVector_Row pivotVecUnit = unit.getRow(pivot);
+			pivotVec.byScalarThis(invPivot);
+			pivotVecUnit.byScalarThis(invPivot);
+			original.setRow(pivot, pivotVec);
+			unit.setRow(pivot, pivotVecUnit);
+			for (int i = 0; i < pivot; i++) {
+				if (i != pivot) {
+					CVector_Row targetVec = original.getRow(i);
+					CVector_Row targetVecUnit = unit.getRow(i);
+					double value = targetVec.getValue(pivot);
+					CVector_Row tmpVec = pivotVec.byScalar(value);
+					CVector_Row subVec = targetVec.subtractVec(tmpVec);
+					CVector_Row tmpVecUnit = pivotVecUnit.byScalar(value);
+					CVector_Row subVecUnit = targetVecUnit.subtractVec(tmpVecUnit);
+					original.setRow(i, subVec);
+					unit.setRow(i, subVecUnit);
+				} // end of if()
+			} //end of for(int i=0...
+		} //end of for( int pivot = 0...
+		return unit;
+	} //end of inverse()
+
+	// この行列の第p列と第q列を入れ替える
+	public void exchangeCol(int p, int q) {
 		CVector_Col tmp = this.getCol(p);
 		this.setCol(p, this.getCol(q));
 		this.setCol(q, tmp);
 	}
-	//この行列の第 p 行と第 q 行を入れ替える
-	public void exchangeRow(int p,int q) {
+
+	// この行列の第 p 行と第 q 行を入れ替える
+	public void exchangeRow(int p, int q) {
 		CVector_Row tmp = this.getRow(p);
 		this.setRow(p, this.getRow(q));
 		this.setRow(q, tmp);
 	}
-	//getter
-	public double[][] getMat(){
+
+	// getter
+	public double[][] getMat() {
 		return this.mat;
 	}
-	//第(i,j)要素を返す
-	public double getValue(int i,int j) {
+
+	// 第(i,j)要素を返す
+	public double getValue(int i, int j) {
 		return this.mat[i][j];
 	}
-	//第j列を列ベクトルとして返す
+
+	// 第j列を列ベクトルとして返す
 	public CVector_Col getCol(int j) {
 		CVector_Col r = new CVector_Col(getColArray(j));
 		return r;
 	}
-	//第j列を double[] で返す
+
+	// 第j列を double[] で返す
 	public double[] getColArray(int j) {
 		double[] r = new double[this.rowN];
-		for(int i=0;i<r.length;i++) {
+		for (int i = 0; i < r.length; i++) {
 			r[i] = this.mat[i][j];
 		}
 		return r;
 	}
-	//第i行を行ベクトルとして返す
+
+	// 第i行を行ベクトルとして返す
 	public CVector_Row getRow(int i) {
 		CVector_Row r = new CVector_Row(getRowArray(i));
 		return r;
 	}
-	//第i行を double[] で返す
+
+	// 第i行を double[] で返す
 	public double[] getRowArray(int i) {
 		double[] r = new double[this.colN];
-		for(int j=0;j<r.length;j++) {
+		for (int j = 0; j < r.length; j++) {
 			r[j] = this.mat[i][j];
 		}
 		return r;
 	}
-	//正方行列かどうか？
+
+	// 正方行列かどうか？
 	public boolean isSquare() {
 		return this.isSquare;
 	}
-	//setter
-	//(i,j)要素に値をセットする
+
+	// setter
+	// (i,j)要素に値をセットする
 	public void setValue(int i, int j, double v) {
 		this.mat[i][j] = v;
 	}
-	//第i 行を CVector_Row で置き換える
+
+	// 第i 行を CVector_Row で置き換える
 	public void setRow(int i, CVector_Row in) {
-		for(int j=0;j<this.colN;j++) {
+		for (int j = 0; j < this.colN; j++) {
 			this.mat[i][j] = in.getArray()[j];
 		}
 	}
-	//第i行を double[] で置き換える。
+
+	// 第i行を double[] で置き換える。
 	public void setRow(int i, double[] in) {
-		for(int j=0;j<this.colN;j++) {
+		for (int j = 0; j < this.colN; j++) {
 			this.mat[i][j] = in[j];
 		}
 	}
-	//第j列を CVector_Colで置き換える
+
+	// 第j列を CVector_Colで置き換える
 	public void setCol(int j, CVector_Col in) {
-		for(int i=0;i<this.rowN;i++) {
+		for (int i = 0; i < this.rowN; i++) {
 			this.mat[i][j] = in.getArray()[i];
 		}
 	}
-	//第j列を double[] で置き換える
+
+	// 第j列を double[] で置き換える
 	public void setCol(int j, double[] in) {
-		for(int i=0;i<this.rowN;i++) {
+		for (int i = 0; i < this.rowN; i++) {
 			this.mat[i][j] = in[i];
 		}
 	}
-	
+
 }
